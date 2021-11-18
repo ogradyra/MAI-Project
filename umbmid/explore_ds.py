877e385835b58ee5914571ec8fbc6a36f3e1aeff
Rachel O'Grady <ogradyra@tcd.ie>
2021-11-18 23:55:36 +0000

    add umbmid

---
+++ umbmid/explore_ds.py
@@ -0,0 +1,13 @@
+import pickle
+import pandas as pd
+
+# file containing the measured frequency-domain S11 parameters of all scans in that generation of the dataset, 
+# after having performed empty-chamber reference subtraction
+#infile = open('metadata_gen_one.pickle', 'rb')
+#new_dict = pickle.load(infile)
+#infile.close()
+
+data = pd.read_pickle("./metadata_gen_one.pickle")
+print(data)
+
+#print(new_dict)
