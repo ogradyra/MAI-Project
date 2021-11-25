5a2757ea6fa5eb6c6859788b1a160040f9c6a332
Rachel O'Grady <90624141+ogradyra@users.noreply.github.com>
2021-11-25 10:57:43 +0000

    Update explore_ds.py

--- umbmid/explore_ds.py
+++ umbmid/explore_ds.py
@@ -3,11 +3,11 @@ import pandas as pd
 
 # file containing the measured frequency-domain S11 parameters of all scans in that generation of the dataset, 
 # after having performed empty-chamber reference subtraction
-#infile = open('metadata_gen_one.pickle', 'rb')
-#new_dict = pickle.load(infile)
-#infile.close()
+infile = open('umbmid/metadata_gen_one.pickle', 'rb')
+new_dict = pickle.load(infile)
+infile.close()
 
-data = pd.read_pickle("./metadata_gen_one.pickle")
-print(data)
+#data = pd.read_pickle("metadata_gen_one.pickle")
+#print(data)
 
-#print(new_dict)
+print(new_dict[151])
