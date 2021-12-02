86db2fc3d64700b8a4bb2a12ea1a447d6ddd3079
Rachel O'Grady <ogradyra@tcd.ie>
2021-12-02 23:19:14 +0000

    update umbmid

--- umbmid/explore_ds.py
+++ umbmid/explore_ds.py
@@ -6,12 +6,12 @@ import pandas as pd
 
 f1 = open('umbmid/td_cal_data.pickle', 'rb')
 f2 = open('umbmid/fd_data_gen_two_s11.pickle', 'rb')
-#infile = open('umbmid/geom_params.pickle','rb')
+#f4 = open('umbmid/geom_params.pickle','rb')
 f3 = open('umbmid/metadata_gen_two.pickle', 'rb')
 
-dict1 = pickle.load(f1)
-dict2 = pickle.load(f2)
-dict3 = pickle.load(f3)
+itdas = pickle.load(f1)
+gen2 = pickle.load(f2)
+md_gen2 = pickle.load(f3)
 
 f1.close()
 f2.close()
@@ -20,28 +20,30 @@ f3.close()
 #data = pd.read_pickle("umbmid/fd_data.pickle")
 #print(data)
 
-d1 = dict1['c2sf3cm']
-d2 = dict2[989]
-tar = dict2[989, :, :]
-emp = dict2[993, :, :]
+# data from one of the itdas dataset scans
+d1 = itdas['c2sf3cm']
+
+# data from umbmid scan 990
+d2 = gen2[989]
+
+# phantom scan data 
+tar = gen2[997, :, :]
+# corresponding empty-chamber data for that scan
+emp = gen2[993, :, :]
+# calibrate the target measurements by subtracting the empty-chamber scan data
 cal = tar - emp
 
 #print("Scan with tumour: ", new_dict[989])
 #print("Scan without tumour: ", new_dict[997])
-#print(new_dict['c2sf3cm'])
-#print("DAS data length: ", len(d1))
-#print("UMBMID data length: ", len(d2))
-#print(dict1['c2sf3cm'])
-#print(dict2[989, 0, :])
 
 #print("Num scans umbmid: ", len(dict2[0]))
 #print("Num scans itDAS: ", len(dict1['c2sf3cm']))
 #print("Num frequency points: ", len(dict2[0]))
 #print("Num antenna points: ", len(dict2[0,0]))
 
-print(dict3[989])
+# metadata for scan 990
+print(md_gen2[997])
 
-#print(dict1['c2sf3cm'])
-#print(dict3)
-#with open('./scan_990.pickle', 'wb') as handle:
-#      pickle.dump(cal, handle, protocol=pickle.HIGHEST_PROTOCOL)
+# load scan data or metadata into a pickle file
+with open('./scan_998.pickle', 'wb') as handle:
+      pickle.dump(cal, handle, protocol=pickle.HIGHEST_PROTOCOL)
